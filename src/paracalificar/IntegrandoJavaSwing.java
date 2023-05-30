e28d24ee7172862f283140a61daa37a33369a810
JP <ingejp@outlook.com>
2023-05-30 07:17:57 -0600

    Se agrega JPANEL Form para integrar clases con interfaz grafica

---
+++ src/paracalificar/IntegrandoJavaSwing.java
@@ -0,0 +1,204 @@
+
+package paracalificar;
+
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JP
+ */
+public class IntegrandoJavaSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("UnusedAssignment")
+    
+    public static void main(String[] args) {
+       Scanner sn = new Scanner(System.in);
+       NewJFrame jf = new NewJFrame();
+       jf.setVisible(true);       
+      // Calculadora calculadoraEspecial = new Calculadora("JP");
+       
+       //haciendo que el usuario ingrese sus datos para la calculadora apra que sean utilizados en la creacion de la calculadora especial
+       CalculadoraEspecial calculadoraEspecial;
+       System.out.println("Que tipo de Calculadora desea? ");
+       String nomCalculadora = sn.nextLine();
+       System.out.println("Ingrese el color de la Calculadora: ");
+       String colCalculadora = sn.nextLine();
+       System.out.println("Ingrese la dimension de la pantalla de la Calculadora: ");
+       int dimensionPantalla = sn.nextInt();
+       sn.nextLine();
+       System.out.println("Ingrese el numero de serie de Calculadora: ");
+       String numSerie = sn.nextLine();
+       System.out.println("Ingrese su Nombre como Propietario: ");
+       String nomUsuario = sn.nextLine();
+       System.out.println("");
+       System.out.println("=======================");
+       calculadoraEspecial = new CalculadoraEspecial(nomCalculadora, colCalculadora, dimensionPantalla, numSerie, nomUsuario);
+
+       boolean salir = false;
+       int opcion, opcioncalculadora, num1,  num2; 
+         
+       //mostrar los datos
+       calculadoraEspecial.mostrarDatos();
+       System.out.println("=======================");
+       do{
+        
+           System.out.println("");
+           System.out.println("Bienvenidos a si sistema de calculadora preferido.");
+           System.out.println("Mi Nombre es JACK!");
+           System.out.println("Por favor, seleccione la calculadora a utilizar");
+           System.out.println("");
+           System.out.println("=======================");
+           System.out.println("1. Calculadora Simple");
+           System.out.println("2. Calculadora Especial");
+           System.out.println("3. Salir");
+           System.out.println("=======================");
+           
+           opcioncalculadora = sn.nextInt();
+           
+           switch(opcioncalculadora){
+               case 1:
+                  System.out.println("");
+                  System.out.println("=======================");
+                  System.out.println("1. Sumar");
+                  System.out.println("2. Restar");
+                  System.out.println("3. Multiplicar");
+                  System.out.println("4. Dividir");
+                  System.out.println("5. Raiz Cuadrada");
+                  System.out.println("6. Verificar numero Primo");
+                  System.out.println("7. Salir");
+                  System.out.println("Selecciona una opcion");
+                  System.out.println("=======================");
+                  
+                  opcion = sn.nextInt();
+                   
+              switch(opcion){
+                  case 1:  
+                      System.out.println("Ingrese el primer numero: ");
+                      num1 = sn.nextInt();
+                      
+                      System.out.println("Ingrese el segundo numero: ");
+                      num2 = sn.nextInt();
+                      
+                      System.out.println("El resultado de la suma es = " + calculadoraEspecial.sumar(num1, num2));
+                      break;
+                  case 2:
+                      System.out.println("Ingrese el primer numero: ");
+                      num1 = sn.nextInt();
+                      System.out.println("Ingrese el segundo numero: ");
+                      num2 = sn.nextInt();
+                      System.out.println("El resultado de la resta es = " + calculadoraEspecial.restar(num1, num2));
+                      break;
+                  case 3:
+                      System.out.println("Ingrese el primer numero: ");
+                      num1 = sn.nextInt();
+                      System.out.println("Ingrese el segundo numero: ");
+                      num2 = sn.nextInt();
+                      System.out.println("El resultado de la multiplicacion es = " + calculadoraEspecial.multiplicar(num1, num2));
+                      break;
+                  case 4:
+                      System.out.println("Ingrese el primer numero: ");
+                      num1 = sn.nextInt();
+                      System.out.println("Ingrese el segundo numero: ");
+                      num2 = sn.nextInt();
+                      System.out.println("El resultado de la división es = " + calculadoraEspecial.dividir(num1, num2));
+                      break;
+                  case 5:
+                      System.out.println("Ingrese el numero: ");
+                      num1 = sn.nextInt();
+                      System.out.println("El resultado de la raíz cuadrada es = " + calculadoraEspecial.raizCuadrada(num1));
+                      break;
+                  case 6:
+                      System.out.println("Ingrese el numero: ");
+                      num1 = sn.nextInt();
+                      System.out.println("Es primo? = " + calculadoraEspecial.esPrimo(num1));
+                      break;
+                  case 7:
+                      salir=true;
+                  default:
+                      System.out.println("Solo números entre 1 y 7");
+                 
+                }
+              break;
+              
+              case 2:
+                    System.out.println("");
+                    System.out.println("=======================");
+                    System.out.println("1. Generar numero aleatorio");
+                    System.out.println("2. Calcular potencia");
+                    System.out.println("3. Calcular logaritmo");
+                    System.out.println("4. Calcular seno");
+                    System.out.println("5. Calcular coseno");
+                    System.out.println("6. Calcular tangente");
+                    System.out.println("7. Calcular raiz cubica");
+                    System.out.println("8. Salir");
+                    System.out.println("Selecciona una opcion");
+                    System.out.println("=======================");
+                    opcion = sn.nextInt();
+                    switch (opcion) {
+                        case 1: {
+                            System.out.println("Ingrese el limite superior: ");
+                            num1 = sn.nextInt();
+                            System.out.println("Numero aleatorio generado = " + calculadoraEspecial.generarNumeroAleatorio(num1));
+                            break;
+                        }
+                        case 2:{
+                            System.out.println("Ingrese la base: ");
+                            num1 = sn.nextInt();
+                            System.out.println("Ingrese el exponente: ");
+                            num2 = sn.nextInt();
+                            System.out.println("El resultado de la potencia es = " + calculadoraEspecial.calcularPotencia(num1, num2));
+                            break;
+                        }
+                        case 3: {
+                            System.out.println("Ingrese el numero: ");
+                            num1 = sn.nextInt();
+                            System.out.println("El resultado del logaritmo es = " + calculadoraEspecial.calcularLogaritmo(num1));
+                            break;
+                        }
+                        case 4: {
+                            System.out.println("Ingrese el angulo en radianes: ");
+                            num1 = sn.nextInt();
+                            System.out.println("El resultado del seno es = " + calculadoraEspecial.calcularSeno(num1));
+                            break;
+                        }
+                        case 5: {
+                            System.out.println("Ingrese el angulo en radianes: ");
+                            num1 = sn.nextInt();
+                            System.out.println("El resultado del coseno es = " + calculadoraEspecial.calcularCoseno(num1));
+                            break;
+                        }
+                        case 6:{
+                            System.out.println("Ingrese el angulo en radianes: ");
+                            num1 = sn.nextInt();
+                            System.out.println("El resultado de la tangente es = " + calculadoraEspecial.calcularTangente(num1));
+                            break;
+                        }
+                        case 7: {
+                            System.out.println("Ingrese el numero: ");
+                            num1 = sn.nextInt();
+                            System.out.println("El resultado de la raiz cubica es = " + calculadoraEspecial.calcularRaizCubica(num1));
+                            break;
+                        }
+                         case 8: 
+                            salir = true;
+                         default:
+                            System.out.println("Solo numeros entre 1 y 8");
+                    }
+                break;
+    
+                case 3:
+                    salir=true;
+            }  
+        }
+    
+        while(!salir);
+       System.out.println("Hasta luego!");
+        
+    }
+    
+}
---
+++ src/paracalificar/NewJFrame.java
@@ -0,0 +1,192 @@
+package paracalificar;
+
+public class NewJFrame extends javax.swing.JFrame {
+    CalculadoraEspecial ce =new CalculadoraEspecial("Especial", "verde", 10, "abs123", "JP");
+    
+    public NewJFrame() {
+        initComponents();
+        jTextField1.requestFocus();
+    }
+    
+      
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("PrograIII con JP");
+
+        jLabel1.setText("Primer Numero");
+
+        jTextField1.setColumns(20);
+
+        jLabel2.setText("Segundo Numero");
+
+        jTextField2.setColumns(20);
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton1.setText("Sumar");
+        jButton1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jButton1MouseDragged(evt);
+            }
+        });
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
+
+        jLabel3.setText("Resultado");
+
+        jTextField3.setColumns(12);
+        jTextField3.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton2.setText("CE");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
+
+        jButton3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton3.setText("Pow");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
+
+        jButton4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jButton4.setText("Logaritmo");
+        jButton4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton4MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(76, 76, 76)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(89, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(51, 51, 51)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3))
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton4)
+                .addContainerGap(31, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseDragged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1MouseDragged
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+       double primerNumero=Double.parseDouble(jTextField1.getText());
+       double segundoNumero=Double.parseDouble(jTextField2.getText());
+       double resultado= ce.sumar(primerNumero, segundoNumero);
+       jTextField3.setText(String.valueOf(resultado)); 
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        this.jTextField1.setText("");
+        this.jTextField2.setText("");
+        this.jTextField3.setText("");
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+        double primerNumero=Double.parseDouble(jTextField1.getText());
+       double segundoNumero=Double.parseDouble(jTextField2.getText());
+       double resultado= ce.calcularPotencia(primerNumero, segundoNumero);
+        jTextField3.setText(String.valueOf(resultado));
+    }//GEN-LAST:event_jButton3MouseClicked
+
+    private void jButton4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton4MouseClicked
+        double primerNumero=Double.parseDouble(jTextField1.getText());
+       double resultado= ce.calcularLogaritmo(primerNumero);
+        jTextField3.setText(String.valueOf(resultado));
+    }//GEN-LAST:event_jButton4MouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);
+            }
+             
+        });
+        
+       
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    // End of variables declaration//GEN-END:variables
+}
